c4289e5a787ac8469632ae6027458e2538bf97df
Naniram-code <NaniramBhujel@gmail.com>
2023-05-17 00:05:52 -0400

    updated

--- src/main/java/JDBCDemoExample.java
+++ src/main/java/JDBCDemoExample.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class JDBCDemoExample {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCDemoExample {
+
+
+
+        public static void main(String[] args) {
+            String url = "jdbc:mysql://localhost:3306/InsuranceMangementSystem";
+            String username = "root";
+            String password = "Bex987174";
+
+            try (Connection connection = DriverManager.getConnection(url, username, password)) {
+                System.out.println("Connection established successfully");
+                // Perform database operations here
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
